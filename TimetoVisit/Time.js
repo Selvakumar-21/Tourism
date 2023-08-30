eba9313a57685efb7bc5f83769dc54c7ac03b881
Selvakumar N <124679903+Selvakumar-21@users.noreply.github.com>
2023-08-30 14:58:31 +0530

    Add files via upload

---
+++ TimetoVisit/Time.js
@@ -0,0 +1,34 @@
+import React from "react";
+import TimetoVisit from '../TimetoVisit/TimetoVisit';
+import Chennai from '../TimetoVisit/Chennai';
+import Rameswaram from '../TimetoVisit/Rameswaram';
+import Ooty from '../TimetoVisit/Ooty';
+import Kumbakonam from '../TimetoVisit/Kumbakonam';
+import Yercaud from '../TimetoVisit/Yercaud';
+import Hogenakkal from '../TimetoVisit/Hogenakkal';
+import Kodaikanal from '../TimetoVisit/Kodaikanal';
+import Kanyakumari from '../TimetoVisit/Kanyakumari';
+import Theni from '../TimetoVisit/Theni';
+import Madurai from '../TimetoVisit/Madurai';
+
+
+const Time = () => {
+  return (
+    <div>
+        <TimetoVisit />
+        <Chennai />
+        <Rameswaram />
+        <Kodaikanal />
+        <Ooty />
+        <Kanyakumari />
+        <Kumbakonam />
+        <Madurai />
+        <Yercaud />
+        <Theni />
+        <Hogenakkal />
+        
+    </div>
+  )
+}
+
+export default Time;
