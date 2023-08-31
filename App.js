74929643bd944d4e6af3979d09d661a0de52f964
Selvakumar N <124679903+Selvakumar-21@users.noreply.github.com>
2023-08-31 22:04:17 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import PlaceToVisit from "./components/PlaceToVisit";
 import Time from "./components/TimetoVisit/Time";
 import Nav from "./Nav";
 import Place from "./Place";
+import Food from "./components/Food/Food";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path='/' element={<Place/> } />
         <Route path="/PlaceToVisit" element={<PlaceToVisit /> } />
         <Route path="/TimetoVisit" element={<Time />} />
+        <Route path="/Food" element={<Food />} />
       </Routes>
     </div>
   );
